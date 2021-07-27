648e4b5aebecbd3314d8278ff38c7442b08aa0f5
Preston Davis <prem.dav@outlook.com>
2021-07-27 05:42:16 -0500

    adds training method

--- neural-network-fs/mnist.py
+++ neural-network-fs/mnist.py
@@ -7,11 +7,31 @@ from nn import Layer, Network, LeakyReLU, MSE
 
 def load_data(path):
   with open(path, 'r') as f:
-    data = np.asarray(list(csv.reader(f)))
+    data = np.asarray(list(csv.reader(f)), dtype=float)
   return data
 
 def to_col(array):
   return array.reshape((array.size, 1))
+
+def test(net, test_data):
+  '''compute accuracy of data'''
+  hits = 0
+  for row in test_data:
+    x = to_col(row[1:])
+    out = net.forward_pass(x)
+    guess = np.argmax(out)
+    if guess == row[0]:
+      hits += 1
+  return hits/test_data.shape[0]
+
+def train(net, train_data):
+  '''train network on given training data'''
+  ts = np.eye(10)
+  for row in train_data:
+    digit = row[0]
+    x = to_col(row[1:])
+    t = to_col(ts[digit, :])
+    net.train(x, t)
 
 if __name__ == '__main__':
   layers = [
@@ -22,6 +42,9 @@ if __name__ == '__main__':
   net = Network(layers, 0.001, MSE())
 
 # test network
+test_data = load_data('mnistdata/mnist_test.csv')
+accuracy = test(net, test_data)
+print(f'accuracy: {round(100 * accuracy, 2)}%')
 
 # train network
 
