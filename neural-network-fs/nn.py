f53201d05586fe3b71242c10eedd55095a2935f8
Preston Davis <prem.dav@outlook.com>
2021-07-27 04:31:23 -0500

    implement loss function

--- neural-network-fs/nn.py
+++ neural-network-fs/nn.py
@@ -10,15 +10,22 @@ def generate_weight_matrix(nrows, ncols):
 def generate_bias_factor(length):
   return generate_weight_matrix(length, 1)
 
+def leaky_relu(x, alpha=0.1):
+  return np.maximum(x, alpha*x)
+
+def mean_squared_error(outs, targets):
+  return np.mean(np.power(outs - targets, 2))
+
 class Layer:
   '''Class representing connections between two layers of neurons'''
 
-  def __init__(self, inps, outs):
+  def __init__(self, inps, outs, act_func):
       self._W = generate_weight_matrix(outs, inps)
       self._b = generate_bias_factor(outs)
+      self._f = act_func
 
   def forward_pass(self, x):
-    return np.dot(self._W, x) + self._b
+    return self._f(np.dot(self._W, x) + self._b)
 
 class Network:
   '''Class representing sequence of compatible layers'''
@@ -33,9 +40,9 @@ class Network:
 
 if __name__ == '__main__':
   layers = [
-    Layer(3, 7),
-    Layer(7, 6),
-    Layer(6, 2),
+    Layer(3, 7, leaky_relu),
+    Layer(7, 6, leaky_relu),
+    Layer(6, 2, leaky_relu),
   ]
 
   net = Network(layers)
